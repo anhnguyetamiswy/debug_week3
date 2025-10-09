88c0b57b46e2934dadd5d725ff6f0bab0b387afb
anhnguyetamiswy <anhnguyet10042006@gmail.com>
2025-10-09 21:49:05 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+    struct SinhVien {
+    int mssv;
+    char hoTen[50];
+    double diemTB;
+    };
+    int main() {
+    //struct SinhVien sv1 = { 1234, "Nguyen Van A", 8.0};
+    struct SinhVien sv2;
+    ptintf("\n hay nhap thong tin sinh vien 2");
+    printf("\n Hay nhap ho ten sinh vien 2: ");
+    fgets(sv2.hoTen, 50, stdin);
+    printf("\n Hay nhap mssv: ");
+    scanf("%d", &sv2.mssv);
+    printf("\n Hay nhap diem trung binh: ");
+    scanf("%lf", &sv2.diemTB);
+    return 0;
+    }
